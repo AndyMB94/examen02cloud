4a13d3faa56fb21f6fd5c5d9cccf0bb73ae05d61
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2025-05-07 02:08:15 -0500

    Registrar modelos Cliente y Producto en el admin de Django

--- backend/clientes/admin.py
+++ backend/clientes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Cliente
 
-# Register your models here.
+class ClienteAdmin(UserAdmin):
+    model = Cliente
+    list_display = ('username', 'email', 'nombre', 'celular', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información adicional', {'fields': ('nombre', 'celular')}),
+    )
+
+admin.site.register(Cliente, ClienteAdmin)
