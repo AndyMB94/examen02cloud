77a1cf46b327da6e41b4a09be65defc2dbb40356
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2025-05-06 23:36:04 -0500

    modificacion de la db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'miapp',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'db',      # Muy importante: debe ser "db" porque así se llama el contenedor en docker-compose
+        'PORT': '5432',
     }
 }
 
